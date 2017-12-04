951b2e88ec8d8a5a7df6235b31242513acd1a404
Albert Nazander <alizbazar@alizweb.com>
2017-12-04 20:16:12 +0200

    Add logic to update listeners once props change

--- src/withLoader.js
+++ src/withLoader.js
@@ -10,44 +10,76 @@ export default function (paths, options) {
     class WithData extends Component {
       constructor (props) {
         super(props)
-        this.listeners = []
-        this.isAttached = true
-      }
 
-      componentWillMount () {
-        const sources = (typeof paths === 'function') ? paths(this.props) : paths
-        const loaderOptions = Object.assign({},
-          options,
-          { componentName: wrappedComponentName },
-        )
-        Object.keys(sources).forEach(path => {
-          const pathOptions = sources[path]
-          attachDataLoader(path, pathOptions, loaderOptions).then(unsubscribe => {
-            if (typeof unsubscribe === 'function') {
-              if (this.isAttached) {
-                this.listeners.push(unsubscribe)
-              } else {
-                // Unsubscribe immediately if the component is not mounted
-                unsubscribe()
-              }
-            }
-          })
+        this.loaders = {}
+        this.isAttached = true
+
+        this.loaderOptions = Object.assign({}, options, {
+          componentName: wrappedComponentName,
         })
       }
 
-      componentWillUnmount() {
-        let unsubscriber = this.listeners.pop()
-        while (unsubscriber) {
-          unsubscriber()
-          unsubscriber = this.listeners.pop()
+      componentWillMount () {
+        this.attachLoaders(this.props)
+      }
+
+      componentWillReceiveProps (newProps) {
+        if (typeof paths !== 'function') {
+          // sources cannot change if they are given as object
+          return
         }
+        this.attachLoaders(newProps)
+      }
+
+      componentWillUnmount() {
+        Object.keys(this.loaders).forEach(path => {
+          if (typeof this.loaders[path] === 'function') {
+            this.loaders[path]()
+          }
+        })
+        this.loaders = {}
         this.isAttached = false
+      }
+
+      attachLoaders (props) {
+        const expiredPaths = Object.keys(this.loaders)
+        const sources = (typeof paths === 'function') ? paths(props) : paths
+        Object.keys(sources).forEach(path => {
+          if (this.loaders[path]) {
+            // loader exists -> it's not expired
+            const i = expiredPaths.indexOf(path)
+            expiredPaths.splice(i, 1)
+          } else {
+            // new loader added -> attach it
+            const pathOptions = sources[path]
+            this.loaders[path] = true
+            attachDataLoader(path, pathOptions, this.loaderOptions).then(unsubscribe => {
+              if (typeof unsubscribe === 'function') {
+                if (this.isAttached) {
+                  this.loaders[path] = unsubscribe
+                } else {
+                  // Unsubscribe immediately if the component is not mounted
+                  unsubscribe()
+                }
+              }
+            })
+          }
+        })
+
+        // The rest loaders are no longer relevant -> remove them
+        expiredPaths.forEach(path => {
+          if (typeof this.loaders[path] === 'function') {
+            this.loaders[path]()
+          }
+          delete this.loaders[path]
+        })
       }
 
       render () {
         return createElement(WrappedComponent, this.props)
       }
     }
+
 
     WithData.displayName = `withData(${wrappedComponentName})`
 
